2fd062b730f80b2c92f86e261d4b12ed46a662ea
Ignacio Fonsalido <iguifon@hotmail.com>
2021-08-10 23:34:16 -0300

    segundo

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+const App = () => {
+
+return (
+     <>
+        <header>
+                 <h1>VinotecaOlmos</h1>
+                   <nav>
+<a href="">Historia</a>
+<a href="">Productos</a>
+<a href="">Contacto</a>
+                   </nav>                
+        </header>
+     </>
+)
+}
+
+export default App
